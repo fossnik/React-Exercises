695ebf09943cb07d9936cc87cc7a3893f7a38a8e
Zack Hartmann <zackhartmann@gmail.com>
2018-04-25 16:20:28 -0400

    Implemented Console-Logging Event Handler

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	render() {
-		return <input/>;
+		return <input onChange={this.onInputChange}/>;
+	}
+
+	// event object is 'event'
+	onInputChange(event) {
+		console.log(event.target.value)
 	}
 }
 
