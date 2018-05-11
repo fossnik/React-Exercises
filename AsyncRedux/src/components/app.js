9c029ff9517c4e2582395e962335882a93b2a45f
Zack Hartmann <zackhartmann@gmail.com>
2018-05-11 16:23:51 -0400

    Implemented Input Form

--- AsyncRedux/src/components/app.js
+++ AsyncRedux/src/components/app.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 import SearchBar from '../containers/search_bar'
 
 export default class App extends Component {
-  render() {
-	  return <div>
-		  <SearchBar/>
-	  </div>
-  }
+	render() {
+		return <div>
+			<SearchBar/>
+		</div>
+	}
 }
--- AsyncRedux/src/containers/search_bar.js
+++ AsyncRedux/src/containers/search_bar.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		// defaults to empty string
+		this.state = { term: '' };
+
+		// binds component state to onInputChange method (makes state context accessible)
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({ term: event.target.value })
+	}
+
+	// prevent form submission (because single page application)
+	// this is needed when using a <form> tag
+	static onFormSubmit(event) {
+		event.preventDefault();
+	}
+
 	render() {
-		return <form className="input-group">
-			<input/>
+		return <form onSubmit={SearchBar.onFormSubmit} className="input-group">
+			<input
+				placeholder="Get a five-day forecast in your favorite cities"
+				className="form-control"
+				value={this.state.term}
+				onChange={this.onInputChange}
+				/>
 			<span className="input-group-btn">
 				<button type="submit" className="btn btn-secondary">Submit</button>
 			</span>
