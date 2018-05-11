c54a8348df6d08236e556f93e32319ebd43b74ff
Zack Hartmann <zackhartmann@gmail.com>
2018-05-11 12:14:32 -0400

    Implemented Pages

--- Books/src/containers/book-detail.js
+++ Books/src/containers/book-detail.js
@@ -4,11 +4,12 @@ import { connect } from 'react-redux'
 class BookDetail extends Component {
 	render() {
 		if (!this.props.book)
-			return <div>Select an item to get started</div>
+			return <div>Select an item to get started</div>;
 		else
 			return <div>
 				<h3>Book Detail For:</h3>
-				<div>{this.props.book.title}</div>
+				<div>Title: {this.props.book.title}</div>
+				<div>Pages: {this.props.book.pages}</div>
 			</div>
 	}
 }
