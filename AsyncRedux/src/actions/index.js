733ce13c303349426eb6f82274850331beeab417
Zack Hartmann <zackhartmann@gmail.com>
2018-05-13 12:23:43 -0400

    Implemented Action

--- AsyncRedux/src/actions/index.js
+++ AsyncRedux/src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios'
+
 const API_KEY = '5ecf18f5f1b241c25bba9ba10cc4d173';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forcast?appid=${API_KEY}`;
+
+// helps keep our action types consistent between our action creators and reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		// an action always has to have a type
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
--- AsyncRedux/src/containers/search_bar.js
+++ AsyncRedux/src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from "../actions"
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
@@ -9,16 +12,23 @@ export default class SearchBar extends Component {
 
 		// binds component state to onInputChange method (makes state context accessible)
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(event) {
 		this.setState({ term: event.target.value })
 	}
 
-	// prevent form submission (because single page application)
-	// this is needed when using a <form> tag
-	static onFormSubmit(event) {
+	onFormSubmit(event) {
+		// prevent form submission (because single page application)
+		// this is needed when using a <form> tag
 		event.preventDefault();
+
+		// calls the action mapped in by mapDispatchToProps
+		this.props.fetchWeather(this.state.term);
+
+		// clear search-bar input
+		this.setState({ term: '' });
 	}
 
 	render() {
@@ -35,3 +45,11 @@ export default class SearchBar extends Component {
 		</form>
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// null says "thanks, but we don't care about state"
+// this connects our action creator to this container
+export default connect(null, mapDispatchToProps)(SearchBar)
