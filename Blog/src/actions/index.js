79fddcaa03b65617e1c3050813a7465c6dc5e28b
Zack Hartmann <zackhartmann@gmail.com>
2018-05-24 16:54:46 -0400

    Implemented Post Request

--- Blog/src/actions/index.js
+++ Blog/src/actions/index.js
@@ -1,16 +1,15 @@
 import axios from 'axios'
 
-const API_KEY = '5ecf18f5f1b241c25bba9ba10cc4d173';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forcast?appid=${API_KEY}`;
+export const FETCH_POSTS = 'fetch_posts';
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=PAPERCLIP1234';
 
-export function fetchWeather(city) {
-	const url = `${ROOT_URL}&q=${city},us`;
-	const request = axios.get(url);
+export function fetchPosts() {
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
 	return {
-		type: FETCH_WEATHER,
+		type: FETCH_POSTS,
 		payload: request
 	}
 }
--- Blog/src/index.js
+++ Blog/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter, Route } from 'react-router-dom'
+import ReduxPromise from 'redux-promise'
 
 import reducers from './reducers'
+import PostsIndex from './components/posts_index'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
 		<BrowserRouter>
 			<div>
+				<PostsIndex/>
 			</div>
 		</BrowserRouter>
 	</Provider>
