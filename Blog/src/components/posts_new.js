3b404a0de5aed6381defad4b3f0b2e08c0b09e62
Zack Hartmann <zackhartmann@gmail.com>
2018-05-25 10:26:23 -0400

    Introduced Redux Form Implementation

--- Blog/src/components/posts_new.js
+++ Blog/src/components/posts_new.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
+// very similar to the redux 'connect' helper
+import { Field, reduxForm } from 'redux-form'
+
 class PostsNew extends Component {
 	render() {
-		return <div>
-			Posts New!
-		</div>
+		return <form>
+			<Field
+				name="title"
+				component={}
+			/>
+		</form>
 	}
 }
 
-export default PostsNew
+export default reduxForm({
+	form: 'PostsNewForm'
+})(PostsNew)
--- Blog/src/reducers/index.js
+++ Blog/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
+
+// alias syntax: renaming recommended - "reducer" too generic
+import { reducer as formReducer } from 'redux-form'
 import PostsReducer from './reducer_posts'
 
 const rootReducer = combineReducers({
 	posts: PostsReducer,
+	form: formReducer,
 });
 
 export default rootReducer
