0de10b2b9a6d8bb011c347e5b3ca5269fb86d0d3
Zack Hartmann <zackhartmann@gmail.com>
2018-05-29 12:30:00 -0400

    Implemented Form Cancel Button

--- Blog/src/components/posts_new.js
+++ Blog/src/components/posts_new.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-
-// very similar to the redux 'connect' helper
 import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
 
 class PostsNew extends Component {
 	renderField(field) {
@@ -46,6 +45,7 @@ class PostsNew extends Component {
 				component={this.renderField}
 			/>
 			<button type="submit" className="btn btn-primary">Submit</button>
+			<Link to="/" className="btn btn-danger">Cancel</Link>
 		</form>
 	}
 }
